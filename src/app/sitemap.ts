ef749dddf06e4b52f3261ab89044c314addfc289
Lukas Wiesehan <mail@lukaswiesehan.de>
2024-03-14 13:27:38 +0100

    Update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,58 +1,63 @@
-import {allBlogPosts, allTextPages} from 'contentlayer/generated'
+import {allBlogPosts, allProfiles, allTextPages} from 'contentlayer/generated'
 import {MetadataRoute} from 'next'
-
-//TODO: Re-enable props after Next.js update
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = process.env.NODE_ENV === 'production' ? 'https://lw.works' : 'http://localhost:3000'
   return [
     {
       url: `${baseUrl}/de`,
-      lastModified: new Date()
-      //changeFrequency: 'yearly',
-      //priority: 1
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1
     },
     {
       url: `${baseUrl}/en`,
-      lastModified: new Date()
-      //changeFrequency: 'yearly',
-      //priority: 1
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1
     },
     {
       url: `${baseUrl}/de/work`,
-      lastModified: new Date()
-      //changeFrequency: 'monthly',
-      //priority: 0.8
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8
     },
     {
       url: `${baseUrl}/en/work`,
-      lastModified: new Date()
-      //changeFrequency: 'monthly',
-      //priority: 0.8
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8
     },
     {
       url: `${baseUrl}/de/blog`,
-      lastModified: new Date()
-      //changeFrequency: 'weekly',
-      //priority: 0.5
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5
     },
     {
       url: `${baseUrl}/en/blog`,
-      lastModified: new Date()
-      //changeFrequency: 'weekly',
-      //priority: 0.5
-    },
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5
+    }, // @ts-ignore
+    ...allProfiles.map(({language, slug}) => ({
+      url: `${baseUrl}/${language}/contact/${slug}`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.6
+    })),
+    // @ts-ignore
     ...allBlogPosts.map(({language, slug}) => ({
       url: `${baseUrl}/${language}/blog/${slug}`,
-      lastModified: new Date()
-      //changeFrequency: 'weekly',
-      //priority: 0.5
-    })),
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5
+    })), // @ts-ignore
     ...allTextPages.map(({language, slug}) => ({
       url: `${baseUrl}/${language}/${slug}`,
-      lastModified: new Date()
-      //changeFrequency: 'weekly',
-      //priority: 0.5
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5
     }))
   ]
 }
