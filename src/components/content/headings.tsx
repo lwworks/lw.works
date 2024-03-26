df1333699d831b9d1c59e6fb1ef46693ce2751a2
Lukas Wiesehan <mail@lukaswiesehan.de>
2024-03-26 14:43:44 +0100

    Hide heading links for SRs

--- src/components/content/headings.tsx
+++ src/components/content/headings.tsx
@@ -7,7 +7,7 @@ export const H2: React.FC<React.PropsWithChildren<{}>> = ({children}) => {
   const slug = sluggifyTitle(children)
   return (
     <h2 id={slug} onClick={() => (window.location.hash = `#${slug}`)} className="group cursor-pointer">
-      <span className="absolute left-2 hidden font-mono text-indigo-500 dark:text-indigo-400 md:group-hover:inline lg:left-6">#</span>
+      <span className="absolute left-2 hidden font-mono text-indigo-500 dark:text-indigo-400 md:group-hover:inline lg:left-6 not-sr-only">#</span>
       {children}
     </h2>
   )
@@ -17,7 +17,7 @@ export const H3: React.FC<React.PropsWithChildren<{}>> = ({children}) => {
   const slug = sluggifyTitle(children)
   return (
     <h3 id={slug} onClick={() => (window.location.hash = `#${slug}`)} className="group cursor-pointer">
-      <span className="absolute left-2 hidden font-mono text-indigo-500 dark:text-indigo-400 md:group-hover:inline lg:left-6">#</span>
+      <span className="absolute left-2 hidden font-mono text-indigo-500 dark:text-indigo-400 md:group-hover:inline lg:left-6 not-sr-only">#</span>
       {children}
     </h3>
   )
@@ -27,7 +27,7 @@ export const H4: React.FC<React.PropsWithChildren<{}>> = ({children}) => {
   const slug = sluggifyTitle(children)
   return (
     <h4 id={slug} onClick={() => (window.location.hash = `#${slug}`)} className="group cursor-pointer">
-      <span className="absolute left-2 hidden font-mono text-indigo-500 dark:text-indigo-400 md:group-hover:inline lg:left-6">#</span>
+      <span className="absolute left-2 hidden font-mono text-indigo-500 dark:text-indigo-400 md:group-hover:inline lg:left-6 not-sr-only">#</span>
       {children}
     </h4>
   )
