d3cae8fd3f33a6ae066dd90273da6fb08c8860cc
Lukas Wiesehan <lukas@lw.works>
2024-12-11 15:01:30 +0100

    Log user data after request

--- src/utils/instagram/get-user-data.ts
+++ src/utils/instagram/get-user-data.ts
@@ -1,7 +1,9 @@
 export const getUserData = async (accessToken: string) => {
   console.log(accessToken)
-  const response = await fetch(`https://graph.instagram.com/v21.0/me?fields=id,username,name&access_token=${accessToken}`, {method: 'GET'})
-  const {id, username, name} = await response.json()
+  const response = await fetch(`https://graph.instagram.com/v21.0/me?fields=id,user_id,username,name&access_token=${accessToken}`, {method: 'GET'})
+  const data = await response.json()
+  console.log('user data: ', data)
+  const {id, username, name} = data
   if (!username) throw new Error('Error getting user data.')
   return {scopedId: id, username, name}
 }
