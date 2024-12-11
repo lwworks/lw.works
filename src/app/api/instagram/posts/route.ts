bd59f2de60f5d4d23c24346fdc327629e39c111d
Lukas Wiesehan <lukas@lw.works>
2024-12-11 19:38:16 +0100

    Secure /posts route

--- src/app/api/instagram/posts/route.ts
+++ src/app/api/instagram/posts/route.ts
@@ -5,7 +5,7 @@ import {type NextRequest, NextResponse} from 'next/server'
 
 export async function GET(request: NextRequest) {
   try {
-    // if (request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) return new NextResponse('Unauthorized.', {status: 401})
+    if (request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) return new NextResponse('Unauthorized.', {status: 401})
 
     const searchParams = request.nextUrl.searchParams
     const username = searchParams.get('username')
