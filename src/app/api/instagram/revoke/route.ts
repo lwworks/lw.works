ab493ab9347136a705238db1f145c14f7e013ea4
Lukas Wiesehan <lukas@lw.works>
2024-12-11 14:21:11 +0100

    Update revoke test route

--- src/app/api/instagram/revoke/route.ts
+++ src/app/api/instagram/revoke/route.ts
@@ -1,12 +1,9 @@
 import {NextResponse, type NextRequest} from 'next/server'
 
 export async function POST(request: NextRequest) {
-  const searchParams = request.nextUrl.searchParams
-  console.log('params', searchParams)
-  const body = await request.json()
-  console.log('body', body)
-
   try {
+    const body = await request.json()
+    console.log('body:', body)
     return new NextResponse('Signup successful.', {status: 200})
   } catch (error) {
     console.log(error)
