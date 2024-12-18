b74c39e1fede691d6569f4f8500f1febc690c670
Lukas Wiesehan <lukas@lw.works>
2024-12-18 13:42:27 +0100

    Add title to header

--- src/components/2024/header.tsx
+++ src/components/2024/header.tsx
@@ -1,16 +1,24 @@
-import {Logo} from '@/components/visuals/logo'
-import {ThemeButton} from '@/components/2024/atoms/theme-button'
 import {LanguageButton} from '@/components/2024/atoms/language-button'
+import {ThemeButton} from '@/components/2024/atoms/theme-button'
+import {Logo} from '@/components/visuals/logo'
+import {Locale} from '@/i18n.config'
 import Link from 'next/link'
 import {PrivacyButton} from './atoms/privacy-button'
-import {Locale} from '@/i18n.config'
 
 export const Header = ({lang}: {lang: Locale}) => {
   return (
     <header className="relative py-8 px-4 sm:py-16 lg:py-24 max-w-2xl mx-auto flex justify-between items-center">
-      <Link href="/">
-        <Logo className="h-6 text-black dark:text-white" />
-      </Link>
+      <div className="flex items-center font-mono uppercase gap-2 text-black dark:text-white text-sm">
+        <Link href="/">
+          <Logo className="h-6" />
+        </Link>
+        <span className="text-rose-400">
+          <span className="inline-block -rotate-6">/</span>
+          <span className="inline-block -rotate-6 -ml-0.5">/</span>
+          <span className="inline-block -rotate-6 -ml-0.5">/</span>
+        </span>
+        <span>Integrations</span>
+      </div>
       <div className="shrink-0 flex items-center">
         <LanguageButton />
         <ThemeButton />
