86ae794f955caaa200cf940ed4878b92a4cea4e8
Lukas Wiesehan <lukas@lw.works>
2026-04-02 21:25:23 +0200

    update email template

--- src/emails/digital-dealer-lead.tsx
+++ src/emails/digital-dealer-lead.tsx
@@ -14,29 +14,29 @@ export const DigitalDealerLeadEmail = ({ name, dealership, email, phone, plans }
       <Head />
       <Preview>Neue Anfrage von {name} ({dealership})</Preview>
       <Tailwind>
-        <Body className="bg-white font-sans m-0 py-10">
+        <Body className="bg-white font-sans m-0 p-0">
           <Section className="w-full border-0">
             {/* Logo */}
             <Row>
-              <Column />
+              <Column className="min-w-[24px]" />
               <Column className="w-[480px] border-l border-r border-neutral-200">
                 <Section className="py-7 px-10">
                   <Img
-                    src="https://lw.works/images/logo-black.png"
+                    src="https://2026.lw.works/images/logo-black.png"
                     width="43"
                     height="24"
                     alt="LW Works"
                   />
                 </Section>
               </Column>
-              <Column />
+              <Column className="min-w-[24px]" />
             </Row>
 
             <Hr className="border-neutral-200 m-0" />
 
             {/* Content */}
             <Row>
-              <Column />
+              <Column className="min-w-[24px]" />
               <Column className="w-[480px] border-l border-r border-neutral-200">
                 <Section className="py-8 px-10">
                   <Text className="text-lg font-bold text-neutral-900 m-0 mb-7">Digital Dealer Anfrage</Text>
@@ -61,20 +61,20 @@ export const DigitalDealerLeadEmail = ({ name, dealership, email, phone, plans }
                   <Text className="text-sm text-neutral-900 mt-0.5 mb-0 leading-normal">{plans.length > 0 ? plans.join(', ') : 'Nur Basis'}</Text>
                 </Section>
               </Column>
-              <Column />
+              <Column className="min-w-[24px]" />
             </Row>
 
             <Hr className="border-neutral-200 m-0" />
 
             {/* Footer */}
             <Row>
-              <Column />
-              <Column className="w-[480px] border-l border-r border-neutral-200">
+              <Column className="min-w-[24px] border-b border-neutral-200" />
+              <Column className="w-[480px] border-l border-r border-b border-neutral-200">
                 <Section className="py-5 px-10">
                   <Text className="text-xs text-neutral-400 m-0">&copy; LW Works</Text>
                 </Section>
               </Column>
-              <Column />
+              <Column className="min-w-[24px] border-b border-neutral-200" />
             </Row>
           </Section>
         </Body>
