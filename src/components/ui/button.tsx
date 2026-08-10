acb4b2491f2b2517facf8928ccb73ba822994fe7
Lukas Wiesehan <lukas@lw.works>
2026-08-10 18:55:39 +0200

    update button color

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/80",
+        default: "bg-lime text-black hover:bg-lime/80",
         outline:
           "border-border bg-background/50 hover:bg-muted/50 hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:border-input dark:bg-input/30 dark:hover:bg-input/50",
         secondary:
