b07cd35025186affafc178a9f1b003ed713db44f
Lukas Wiesehan <lukas@lw.works>
2026-08-28 09:11:00 +0200

    make bni logo height responsive

--- src/components/sections/bni/hero.tsx
+++ src/components/sections/bni/hero.tsx
@@ -9,8 +9,8 @@ export const BniHeroSection = () => {
     <Section background="paint-1">
       <Brow color="lavender" className="mb-2">Für BNI-Mitglieder</Brow>
       <Heading as="h1" className="text-3xl sm:text-4xl lg:text-5xl">
-        <Image src="/images/logos/bni-black.svg" alt="BNI Logo" width={167} height={64} className="inline-block dark:hidden h-8 w-auto ml-1 mr-3 -mt-2" />
-        <Image src="/images/logos/bni-white.svg" alt="BNI Logo" width={167} height={64} className="hidden dark:inline-block h-8 w-auto ml-1 mr-3 -mt-2" />
+        <Image src="/images/logos/bni-black.svg" alt="BNI Logo" width={167} height={64} className="inline-block dark:hidden h-5.5 sm:h-6.5 lg:h-8 w-auto ml-1 mr-2 sm:mr-3 -mt-1.5 sm:-mt-2" />
+        <Image src="/images/logos/bni-white.svg" alt="BNI Logo" width={167} height={64} className="hidden dark:inline-block h-5.5 sm:h-6.5 lg:h-8 w-auto ml-1 mr-2 sm:mr-3 -mt-1.5 sm:-mt-2" />
         <span>Vier-Augen-Gespräch<br />mit Lukas Brunkhorst</span>
       </Heading>
       <p className="mt-8 max-w-md text-balance"><span className="font-medium text-black dark:text-white">Lass uns netzwerken!</span> Als BNI-Mitglied kannst Du Dir ganz einfach ein Vier-Augen-Gespräch einbuchen.</p>
