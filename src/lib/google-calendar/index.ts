1b853852010329320fd23f7072e9f18975fffe2d
Lukas Wiesehan <lukas@lw.works>
2026-08-31 08:36:07 +0200

    fix calendar auth?

--- src/lib/google-calendar/index.ts
+++ src/lib/google-calendar/index.ts
@@ -1,13 +1,15 @@
 import {google} from 'googleapis'
 
 export const getCalendarClient = (calendarId: string) => {
+  const isUserCalendar = !calendarId.includes('calendar.google.com')
+
   return google.calendar({
     version: 'v3',
     auth: new google.auth.JWT({
       email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
       key: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY?.replace(/\\n/g, '\n'),
       scopes: ['https://www.googleapis.com/auth/calendar'],
-      subject: calendarId
+      subject: isUserCalendar ? calendarId : undefined
     })
   })
 }
