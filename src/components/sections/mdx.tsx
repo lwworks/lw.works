23c09e97397c853970a633c6f7a60f96691bf825
Lukas Wiesehan <lukas@lw.works>
2026-09-03 08:44:00 +0200

    update privacy policy

--- src/components/sections/mdx.tsx
+++ src/components/sections/mdx.tsx
@@ -45,6 +45,7 @@ export const MDXSection = ({ content: Content, toc, className }: MDXSectionProps
         'prose-headings:font-heading prose-headings:font-black prose-headings:text-black dark:prose-headings:text-white',
         'prose-h2:mt-10 prose-h2:scroll-mt-24 prose-h2:text-3xl prose-h2:first:mt-0',
         'prose-h3:mt-8 prose-h3:scroll-mt-24 prose-h3:text-xl',
+        'prose-h4:font-sans prose-h4:font-semibold',
         'prose-p:text-neutral-600 dark:prose-p:text-neutral-400',
         'prose-strong:font-medium prose-strong:text-black dark:prose-strong:text-white',
         'prose-a:text-black prose-a:underline dark:prose-a:text-white prose-a:hover:opacity-80',
